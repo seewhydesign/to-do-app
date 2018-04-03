c206a2cab7bc2ce567ed75d07cff372d531692cd
Cindy <seewhydesign8@gmail.com>
2018-04-03 15:17:35 -0400

    Assignment 5 completed

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+
+function onDelete(e) {
+  e.target.parentElement.outerHTML = "";
+}
+
+
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
@@ -14,22 +20,36 @@ function onReady() {
     // create a new input
     let checkbox = document.createElement('input');
 
+    // add Delete button
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = onDelete;
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
+
+
 
     // set the title
     newLi.textContent = title;
 
+
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
-    // attache the li to the ul
+    // attach delete button to the li
+    newLi.appendChild(deleteButton);
+
+
+    // attach the li to the ul
     toDoList.appendChild(newLi);
+
 
     // empty the input
     newToDoText.value = '';
   });
 }
+
 
 
 window.onload = function() {
